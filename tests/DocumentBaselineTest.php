3bad9f59fa410e064d300b0029726814dd76ea33
Josh Bruce <josh@8fold.pro>
2021-12-25 12:10:09 -0600

    re-engineer: First test conversion

--- tests/DocumentBaselineTest.php
+++ tests/DocumentBaselineTest.php
@@ -1,73 +1,103 @@
 <?php
+declare(strict_types=1);
+
+namespace Eightfold\HTMLBuilder\Tests;
+
+use PHPUnit\Framework\TestCase;
 
 use Eightfold\HTMLBuilder\Document;
 
 use Eightfold\HTMLBuilder\Element;
 
-test('Docment is stringable', function() {
-    expect(
-        (string) Document::create('title', 'fr', 'ascii')
+class DocumentBaselineTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function document_is_stringable(): void // phpcs:ignore
+    {
+        $expected = <<<html
+            <!doctype html>
+            <html lang="fr"><head><title>title</title><meta charset="ascii"><link rel="stylesheet" href="style.css"><script src="script.js"></script></head><body><p>paragraph content</p></body></html>
+            html;
+
+        $result = (string) Document::create('title', 'fr', 'ascii')
             ->head(
                 Element::link()
                     ->omitEndTag()->props('rel stylesheet', 'href style.css'),
                 Element::script()->props('src script.js')
             )->body(
                 Element::p('paragraph content')
-            )
-    )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
-        '<meta charset="ascii"><link rel="stylesheet" href="style.css">' .
-        '<script src="script.js"></script></head><body><p>paragraph content</p></body></html>'
-    );
-});
+            );
 
-test('Docment can have body', function() {
-    expect(
-        Document::create('title', 'fr', 'ascii')
-            ->head(
-                Element::link()
-                    ->omitEndTag()->props('rel stylesheet', 'href style.css'),
-                Element::script()->props('src script.js')
-            )->body(
-                Element::p('paragraph content')
-            )->build()
-    )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
-        '<meta charset="ascii"><link rel="stylesheet" href="style.css">' .
-        '<script src="script.js"></script></head><body><p>paragraph content</p></body></html>'
-    );
-});
-
-test('Docment can have head', function() {
-    expect(
-        Document::create('title', 'fr', 'ascii')
-            ->head(
-                Element::link()
-                    ->omitEndTag()->props('rel stylesheet', 'href style.css'),
-                Element::script()->props('src script.js')
-            )->build()
-    )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
-        '<meta charset="ascii"><link rel="stylesheet" href="style.css">' .
-        '<script src="script.js"></script></head><body></body></html>'
-    );
-});
-
-test('Docment can change language and character set', function() {
-    expect(
-        Document::create('title', 'fr', 'ascii')->build()
-    )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
-        '<meta charset="ascii"></head><body></body></html>'
-    );
-});
-
-test('Docment has baseline', function() {
-    expect(
-        Document::create('title')->build()
-    )->toBe('<!doctype html>' . "\n" . '<html lang="en"><head><title>title</title>' .
-        '<meta charset="utf-8"></head><body></body></html>'
-    );
-});
-
-test('Document exists', function() {
-    $this->assertTrue(
-        class_exists(Document::class)
-    );
-});
+        $this->assertSame($expected, $result);
+    }
+}
+//
+// test('Docment is stringable', function() {
+//     expect(
+        // (string) Document::create('title', 'fr', 'ascii')
+        //     ->head(
+        //         Element::link()
+        //             ->omitEndTag()->props('rel stylesheet', 'href style.css'),
+        //         Element::script()->props('src script.js')
+        //     )->body(
+        //         Element::p('paragraph content')
+        //     )
+    // )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
+    //     '<meta charset="ascii"><link rel="stylesheet" href="style.css">' .
+    //     '<script src="script.js"></script></head><body><p>paragraph content</p></body></html>'
+//     );
+// });
+//
+// test('Docment can have body', function() {
+//     expect(
+//         Document::create('title', 'fr', 'ascii')
+//             ->head(
+//                 Element::link()
+//                     ->omitEndTag()->props('rel stylesheet', 'href style.css'),
+//                 Element::script()->props('src script.js')
+//             )->body(
+//                 Element::p('paragraph content')
+//             )->build()
+//     )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
+//         '<meta charset="ascii"><link rel="stylesheet" href="style.css">' .
+//         '<script src="script.js"></script></head><body><p>paragraph content</p></body></html>'
+//     );
+// });
+//
+// test('Docment can have head', function() {
+//     expect(
+//         Document::create('title', 'fr', 'ascii')
+//             ->head(
+//                 Element::link()
+//                     ->omitEndTag()->props('rel stylesheet', 'href style.css'),
+//                 Element::script()->props('src script.js')
+//             )->build()
+//     )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
+//         '<meta charset="ascii"><link rel="stylesheet" href="style.css">' .
+//         '<script src="script.js"></script></head><body></body></html>'
+//     );
+// });
+//
+// test('Docment can change language and character set', function() {
+//     expect(
+//         Document::create('title', 'fr', 'ascii')->build()
+//     )->toBe('<!doctype html>' . "\n" . '<html lang="fr"><head><title>title</title>' .
+//         '<meta charset="ascii"></head><body></body></html>'
+//     );
+// });
+//
+// test('Docment has baseline', function() {
+//     expect(
+//         Document::create('title')->build()
+//     )->toBe('<!doctype html>' . "\n" . '<html lang="en"><head><title>title</title>' .
+//         '<meta charset="utf-8"></head><body></body></html>'
+//     );
+// });
+//
+// test('Document exists', function() {
+//     $this->assertTrue(
+//         class_exists(Document::class)
+//     );
+// });
--- tests/ElementBaselineTest.php
+++ tests/ElementBaselineTest.php
@@ -2,40 +2,40 @@
 
 use Eightfold\HTMLBuilder\Element;
 
-test('Element can have empty props', function() {
-    expect(
-        (string) Element::a('link')->props('')
-    )->toBe(<<<html
-        <a>link</a>
-        html
-    );
-});
-
-test('Element has ordered properties', function() {
-    expect(
-        Element::a('link')
-            ->props(
-                'required required',
-                'href https://8fold.pro',
-                'class some-style',
-                'id unique',
-                'data-testing test'
-            )->build()
-    )->toBe(
-        '<a id="unique" class="some-style" href="https://8fold.pro" data-testing="test" required>link</a>'
-    );
-});
-
-test('Element has correct self-closing string', function() {
-    expect(
-        Element::tag()->omitEndTag()->build()
-    )->toBe(
-        '<tag>'
-    );
-});
-
-test('Element exists', function() {
-    $this->assertTrue(
-        class_exists(Element::class)
-    );
-});
+// test('Element can have empty props', function() {
+//     expect(
+//         (string) Element::a('link')->props('')
+//     )->toBe(<<<html
+//         <a>link</a>
+//         html
+//     );
+// });
+//
+// test('Element has ordered properties', function() {
+//     expect(
+//         Element::a('link')
+//             ->props(
+//                 'required required',
+//                 'href https://8fold.pro',
+//                 'class some-style',
+//                 'id unique',
+//                 'data-testing test'
+//             )->build()
+//     )->toBe(
+//         '<a id="unique" class="some-style" href="https://8fold.pro" data-testing="test" required>link</a>'
+//     );
+// });
+//
+// test('Element has correct self-closing string', function() {
+//     expect(
+//         Element::tag()->omitEndTag()->build()
+//     )->toBe(
+//         '<tag>'
+//     );
+// });
+//
+// test('Element exists', function() {
+//     $this->assertTrue(
+//         class_exists(Element::class)
+//     );
+// });
