67ea899605e4379af2e1ff9a304c45f38b4fc238
Josh Bruce <15252830+joshbruce@users.noreply.github.com>
2022-12-10 20:31:06 -0500

    remove: Commented code

--- src/Components/Favicons.php
+++ src/Components/Favicons.php
@@ -16,14 +16,6 @@ use Eightfold\HTMLBuilder\Components\FaviconMetroColors;
 /**
  * We use https://realfavicongenerator.net to generate favicon-related assets.
  * We presume the names of these assets will not be changed.
-
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff">
  */
 class Favicons implements Buildable
 {
