d6fe720654fb74e00724b22b9d572eec37b45139
Josh Bruce <josh@8fold.pro>
2026-08-21 09:58:52 -0400

    BC: Remove dependency on XML builder

--- src/Element.php
+++ src/Element.php
@@ -5,9 +5,9 @@ namespace Eightfold\HTMLBuilder;
 
 use Stringable;
 
-use Eightfold\XMLBuilder\Element as XMLElement;
+// use Eightfold\XMLBuilder\Element as XMLElement;
 
-class Element extends XMLElement implements Stringable
+class Element implements Stringable
 {
     /**
      * The following attributes will be placed in this order if used in an
@@ -51,22 +51,79 @@ class Element extends XMLElement implements Stringable
         "start"
     ];
 
-    protected function omitEndTagClosingString(): string
+    public static function create(string $name, string|Stringable ...$content): static
     {
-        return '>';
+        return new static($name, $content);
+    }
+
+    public static function __callStatic(string $name, array $content = []): static
+    {
+        return new static($name, $content);
+    }
+
+    private function __construct(
+        private string $name,
+        private array $content = [],
+        private array $properties = [],
+        private bool $omitEndTag = false,
+    ) {
+    }
+
+    public function props(string ...$properties): static
+    {
+        $this->properties = $properties;
+        return $this;
+    }
+
+    public function prop(string $prop): static
+    {
+        $this->properties[] = $prop;
+        return $this;
+    }
+
+    public function omitEndTag(): self
+    {
+        $this->omitEndTag = true;
+        return $this;
+    }
+
+    public function toString(): string
+    {
+        return $this->opening()
+            . implode('', array_map('strval', $this->content))
+            . $this->closing();
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    private function opening(): string
+    {
+        if ($this->omitEndTag) {
+            return '<' . $this->name . $this->propertiesString() . '>';
+        }
+
+        return '<' . $this->name . $this->propertiesString() . '>';
+    }
+
+    private function closing(): string
+    {
+        return $this->omitEndTag ? '' : '</' . $this->name . '>';
     }
 
     protected function propertiesString(): string
     {
-        if ($this->properties() === []) {
+        if ($this->properties === []) {
             return '';
         }
 
         $ordered = [];
-        $other = [];
+        $other   = [];
         $boolean = [];
 
-        foreach ($this->properties() as $property) {
+        foreach ($this->properties as $property) {
             [$attr, $value] = array_pad(explode(' ', $property, 2), 2, '');
 
             if ($value === '') {
@@ -75,10 +132,13 @@ class Element extends XMLElement implements Stringable
 
             if (in_array($attr, self::ORDERED, true)) {
                 $ordered[$attr] = $value;
+
             } elseif ($attr === $value) {
                 $boolean[$attr] = true;
+
             } else {
                 $other[$attr] = $value;
+
             }
         }
 
